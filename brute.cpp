e434556f9f39fa5bb798b61914f9dcc14e604d20
jakopako <jakob.dhondt@web.de>
2019-09-02 23:07:03 +0200

    started generate_word_lists

--- brute.cpp
+++ brute.cpp
@@ -4,12 +4,41 @@
 #include <fstream>
 #include <map>
 #include <set>
+#include <algorithm>
 
 using namespace std;
 
 
 class Board {
 
+    map<string, map<string, int>> letter_values = {
+        {"a", {{"points",  1}, {"tiles",  9}}},
+        {"c", {{"points",  3}, {"tiles",  2}}},
+        {"b", {{"points",  3}, {"tiles",  2}}},
+        {"d", {{"points",  2}, {"tiles",  4}}},
+        {"e", {{"points",  1}, {"tiles", 12}}},
+        {"f", {{"points",  4}, {"tiles",  2}}},
+        {"g", {{"points",  2}, {"tiles",  3}}},
+        {"h", {{"points",  4}, {"tiles",  2}}},
+        {"i", {{"points",  1}, {"tiles",  9}}},
+        {"j", {{"points",  8}, {"tiles",  1}}},
+        {"k", {{"points",  5}, {"tiles",  1}}},
+        {"l", {{"points",  1}, {"tiles",  4}}},
+        {"m", {{"points",  3}, {"tiles",  2}}},
+        {"n", {{"points",  1}, {"tiles",  6}}},
+        {"o", {{"points",  1}, {"tiles",  8}}},
+        {"p", {{"points",  3}, {"tiles",  2}}},
+        {"q", {{"points", 12}, {"tiles",  1}}},
+        {"r", {{"points",  1}, {"tiles",  6}}},
+        {"s", {{"points",  1}, {"tiles",  4}}},
+        {"t", {{"points",  1}, {"tiles",  6}}},
+        {"u", {{"points",  2}, {"tiles",  4}}},
+        {"v", {{"points",  6}, {"tiles",  2}}},
+        {"w", {{"points",  5}, {"tiles",  2}}},
+        {"x", {{"points",  7}, {"tiles",  1}}},
+        {"y", {{"points",  5}, {"tiles",  2}}},
+        {"z", {{"points",  8}, {"tiles",  1}}}
+    };
     vector<vector<int> > layout;
     string dict_path;
     vector<string> words;
@@ -20,8 +49,23 @@ class Board {
     map<int, vector<string>> generate_word_lists() {
         map<int, vector<string>> word_lists;
         set<int> word_lengths = extract_word_lengths();
-
+        for (int l: word_lengths) {
+            vector<string> tmp_words;
+            if (l == 1) {
+                for(map<string, map<string, int>>::iterator it = letter_values.begin(); it != letter_values.end(); ++it) {
+                    tmp_words.push_back(it->first);
+                }
+            } else {
+                std::copy_if(words.begin(), words.end(), std::back_inserter(tmp_words), [l](string s){return s.length()==l;} );
+            }
+            sort_by_value(tmp_words);
+            word_lists.insert({l, tmp_words});
+        }
         return word_lists;
+    }
+
+    void sort_by_value(vector<string>& words) {
+
     }
 
     set<int> extract_word_lengths() {
@@ -66,7 +110,6 @@ class Board {
         string word;
         while (getline(in, word))
         {
-            //word.erase( std::remove(word.begin(), word.end(), '\r'), word.end() );
             word.pop_back();
             if(word.size() > 0)
                 w.push_back(word);
@@ -96,14 +139,6 @@ class Board {
 
 int main()
 {
-
-/*     vector<string> msg {"Hello", "C++", "World", "from", "VS Code!", "and the C++ extension!"};
-
-    for (const string& word : msg)
-    {
-        cout << word << " ";
-    }
-    cout << endl; */
     vector<vector<int>> layout1 {
         {1, 1, 1},
         {0, 1, 1},
